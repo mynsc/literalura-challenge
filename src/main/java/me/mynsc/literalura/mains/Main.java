be170a15aa819f62c5c677b790c83eddce7cb6ab
mynsc <mcerquin.10@gmail.com>
2026-01-23 14:55:20 -0500

    Initialize project structure with Spring

---
+++ src/main/java/me/mynsc/literalura/mains/Main.java
@@ -0,0 +1,5 @@
+package me.mynsc.literalura.mains;
+
+public class Main {
+
+}
---
+++ src/test/java/me/mynsc/literalura/LiteraluraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mynsc.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
