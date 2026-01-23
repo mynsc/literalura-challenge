da243163274eba84129b10e45a1aa844fdb81dc4
mynsc <mcerquin.10@gmail.com>
2026-01-23 15:38:54 -0500

    feat: add ApiConsume class for HTTP request handling

---
+++ src/main/java/me/mynsc/literalura/services/ApiConsume.java
@@ -0,0 +1,29 @@
+package me.mynsc.literalura.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiConsume {
+    private HttpClient client;
+
+    public ApiConsume() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String getJson(String url) {
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(url))    
+        .build();
+
+        try {
+            HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
