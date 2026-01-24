f52d83f43f69e70e72e9292717b8b88c94af0f57
mynsc <mcerquin.10@gmail.com>
2026-01-23 20:56:39 -0500

    feat: initialize Main class and invoke showMenu in application startup

--- src/main/java/me/mynsc/literalura/LiteraluraProjectApplication.java
+++ src/main/java/me/mynsc/literalura/LiteraluraProjectApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import me.mynsc.literalura.mains.Main;
+
 @SpringBootApplication
 public class LiteraluraProjectApplication implements CommandLineRunner {
 
@@ -13,7 +15,8 @@ public class LiteraluraProjectApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	
+		Main main = new Main();
+		main.showMenu();
 	}
 
 }
