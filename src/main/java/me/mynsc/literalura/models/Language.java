a09dd6e93757d3a57ddf430d7dab1529e7bdf9a9
mynsc <mcerquin.10@gmail.com>
2026-01-28 16:00:29 -0500

    feat: enhance Language enum to include full names and update constructor

--- src/main/java/me/mynsc/literalura/models/Language.java
+++ src/main/java/me/mynsc/literalura/models/Language.java
@@ -1,14 +1,17 @@
 package me.mynsc.literalura.models;
 
 public enum Language {
-    EN("en"),
-    ES("es"),
-    FR("fr");
+    EN("en", "inglés"),
+    ES("es", "español"),
+    FR("fr", "francés"),
+    UNKNOWN("-", "desconocido");
 
     private String languageGutendex;
+    private String languageFullName;
 
-    Language(String languageGutendex) {
+    Language(String languageGutendex, String languageFullName) {
         this.languageGutendex = languageGutendex;
+        this.languageFullName = languageFullName;
     }
 
     public static Language fromString(String text) {
@@ -17,6 +20,15 @@ public enum Language {
                 return language;
             }
         }
-        throw new IllegalArgumentException("Ningún lenguaje \"" + text + "\" encontrado");
+        return UNKNOWN;
+    }
+
+    public static Language fromLanguageFullName(String text) {
+        for (Language language : Language.values()) {
+            if (language.languageFullName.equalsIgnoreCase(text)) {
+                return language;
+            }
+        }
+        return UNKNOWN;
     }
 }
